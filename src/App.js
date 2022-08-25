955475938396ec9f1350a3df18166dd3f590e242
Wellington Pereira <wellington@mobills.com.br>
2022-08-24 21:41:40 -0300

    initializing project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import api from "./services/api";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get("/users/jwellington58")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Usuário: {user?.login}</p>
+        <p>Biografia: {user?.bio}</p>
     </div>
   );
 }
